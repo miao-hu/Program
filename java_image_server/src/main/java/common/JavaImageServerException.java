36a06ff138dcdcbe388f87b628859d6883dcae91
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-02-18 08:17:46 +0800

    Create JavaImageServerException.java

---
+++ java_image_server/src/main/java/common/JavaImageServerException.java
@@ -0,0 +1,10 @@
+package common;
+
+/**
+ * 创建一个自定义的一个异常类
+ */
+public class JavaImageServerException extends Exception {
+    public JavaImageServerException(String message) {
+        super(message);
+    }
+}
