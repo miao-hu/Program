e8c8fe7788199480d95b65bec9e422886604e685
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-03-02 10:04:41 +0800

    Update ImageDao.java

--- java_image_server/src/main/java/dao/ImageDao.java
+++ java_image_server/src/main/java/dao/ImageDao.java
@@ -9,15 +9,20 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *  用来和数据库打交道的一个类
+ */
+
 public class ImageDao {
+
     /**
-     * 把一个 image 对象插入到数据库中
+     * 把一个 Image 对象插入到数据库中
      */
     public void insert(Image image){
         // 1.获取数据库连接
         Connection connection=DBUtil.getConnection();
         // 2.创建并拼装 SQL 语句
-        String sql="insert into image_table values(null,?,?,?,?,?,?)";
+        String sql="insert into image_table values(null,?,?,?,?,?,?)";  // imageId 是自增主键,不需要赋值
         PreparedStatement statement=null;
         try {
             statement=connection.prepareStatement(sql);
@@ -37,16 +42,17 @@ public class ImageDao {
         } catch (SQLException | JavaImageServerException e) {
             e.printStackTrace();
         }finally {
-            // 4.关闭连接和statement对象
+            // 4.关闭连接和statement对象（无论插入是否成功，都需要关闭连接）
             DBUtil.close(connection,statement,null);
         }
     }
 
     /**
-     * 查找数据库中所有图片的信息
+     * 查找数据库中所有图片的属性信息，并存储在 List 列表中
      */
     public List<Image> selectAll(){
         List<Image> images=new ArrayList<>();
+
         // 1.获取数据库连接
         Connection connection=DBUtil.getConnection();
         // 2.创建并拼装 SQL 语句
@@ -56,9 +62,10 @@ public class ImageDao {
         try {
             // 3.执行 SQL 语句
             statement=connection.prepareStatement(sql);
-            resultSet =statement.executeQuery();
-            // 4.处理结果集 resultSet ，把结果放到我们的返回值当中去
-            while(resultSet.next()){
+            resultSet =statement.executeQuery();  //得到查询结果集
+
+            // 4.处理结果集 resultSet （把结果放到我们的 List 列表当中去）
+            while(resultSet.next()){   //结果集中还有数据
                 Image image=new Image();
                 image.setImageId(resultSet.getInt("imageId"));
                 image.setImageName(resultSet.getString("imageName"));
@@ -67,6 +74,7 @@ public class ImageDao {
                 image.setContentType(resultSet.getString("contentType"));
                 image.setPath(resultSet.getString("path"));
                 image.setMd5(resultSet.getString("md5"));
+
                 images.add(image);
             }
             return images;
@@ -80,7 +88,7 @@ public class ImageDao {
     }
 
     /**
-     * 根据 imageId 查找指定图片的信息
+     * 根据 imageId 查找指定图片的属性信息
      * @param imageId
      * @return
      */
@@ -95,6 +103,74 @@ public class ImageDao {
             // 3.执行 SQL 语句
             statement=connection.prepareStatement(sql);
             statement.setInt(1,imageId);
+
+            resultSet=statement.executeQuery();  //查询
+            // 4.处理结果集
+            if(resultSet.next()){
+                Image image=new Image();
+                image.setImageId(resultSet.getInt("imageId"));
+                image.setImageName(resultSet.getString("imageName"));
+                image.setSize(resultSet.getInt("size"));
+                image.setUploadTime(resultSet.getString("uploadTime"));
+                image.setContentType(resultSet.getString("contentType"));
+                image.setPath(resultSet.getString("path"));
+                image.setMd5(resultSet.getString("md5"));
+
+                return image;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            // 5.关闭连接和statement对象
+            DBUtil.close(connection,statement,resultSet);
+        }
+        return null;   //如果失败的话返回null
+    }
+
+    /**
+     * 根据 imageId 删除指定图片
+     * @param imageId
+     */
+    public void delete(int imageId){
+        // 1. 获取数据库连接
+        Connection connection=DBUtil.getConnection();
+        // 2. 拼装 SQL 语句
+        String sql="delete from image_table where imageId = ?";
+        PreparedStatement statement=null;
+        try {
+            // 3. 执行 SQL 语句
+            statement=connection.prepareStatement(sql);
+            statement.setInt(1,imageId);
+
+            int ret=statement.executeUpdate();  // 删除一条记录，那么影响的行数应该为1
+            if(ret!=1){
+                throw new JavaImageServerException("删除操作出错！");
+            }
+        } catch (SQLException | JavaImageServerException e) {
+            e.printStackTrace();
+        }finally {
+            // 4. 关闭连接
+            DBUtil.close(connection,statement,null);
+        }
+    }
+
+    /**
+     *  按照 md5 的值来查找数据库中的 “图片”Image对象（图片的 md5 值是唯一的）
+     * @param md5
+     * @return
+     */
+    public Image selectByMd5(String md5){
+        // 1.获取数据库连接
+        Connection connection=DBUtil.getConnection();
+        // 2.创建并拼装 SQL 语句
+        String sql="select * from image_table where md5 = ?";
+        PreparedStatement statement=null;
+        ResultSet resultSet=null;
+        try {
+            // 3.执行 SQL 语句
+            statement=connection.prepareStatement(sql);
+            statement.setString(1,md5);    //md5 在数据库中是按字符串形式存储的，所以要调用 setString() 方法
+
             resultSet=statement.executeQuery();
             // 4.处理结果集
             if(resultSet.next()){
@@ -117,33 +193,7 @@ public class ImageDao {
         return null;
     }
 
-    /**
-     * 根据 imageId 删除指定图片的信息
-     * @param imageId
-     */
-    public void delete(int imageId){
-        // 1. 获取数据库连接
-        Connection connection=DBUtil.getConnection();
-        // 2. 拼装 SQL 语句
-        String sql="delete from image_table where imageId = ?";
-        PreparedStatement statement=null;
-        try {
-            // 3. 执行 SQL 语句
-            statement=connection.prepareStatement(sql);
-            statement.setInt(1,imageId);
-            int ret=statement.executeUpdate();
-            if(ret!=1){
-                throw new JavaImageServerException("删除操作出错！");
-            }
-        } catch (SQLException | JavaImageServerException e) {
-            e.printStackTrace();
-        }finally {
-            // 4. 关闭连接
-            DBUtil.close(connection,statement,null);
-        }
-    }
-
-    // 如果你的数据库是云服务器上, 没有在本地服务器上. 这个程序在本地直接运行就会无法访问数据库
+    // 如果你的数据库在云服务器上, 没有在本地服务器上. 这个程序在本地直接运行就会无法访问数据库
     // 此处需要把这个程序部署到云服务器上执行，才能看到效果
     // 打一个 jar 包, 把 jar 包拷贝到云服务器上, 就可以执行了
     public static void main(String[] args) {
@@ -173,45 +223,8 @@ public class ImageDao {
         */
 
         // 4.测试删除图片
-        ImageDao imageDao=new ImageDao();
-        imageDao.delete(1);
-    }
-
-    /**
-     *  按照 md5 来查找数据库中的 图片
-     * @param md5
-     * @return
-     */
-    public Image selectByMd5(String md5){
-        // 1.获取数据库连接
-        Connection connection=DBUtil.getConnection();
-        // 2.创建并拼装 SQL 语句
-        String sql="select * from image_table where md5 = ?";
-        PreparedStatement statement=null;
-        ResultSet resultSet=null;
-        try {
-            // 3.执行 SQL 语句
-            statement=connection.prepareStatement(sql);
-            statement.setString(1,md5);    //md5 在数据库中是按字符串形式存储的
-            resultSet=statement.executeQuery();
-            // 4.处理结果集
-            if(resultSet.next()){
-                Image image=new Image();
-                image.setImageId(resultSet.getInt("imageId"));
-                image.setImageName(resultSet.getString("imageName"));
-                image.setSize(resultSet.getInt("size"));
-                image.setUploadTime(resultSet.getString("uploadTime"));
-                image.setContentType(resultSet.getString("contentType"));
-                image.setPath(resultSet.getString("path"));
-                image.setMd5(resultSet.getString("md5"));
-                return image;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }finally {
-            // 5.关闭连接和statement对象
-            DBUtil.close(connection,statement,resultSet);
-        }
-        return null;
+        /*  ImageDao imageDao=new ImageDao();
+            imageDao.delete(1);
+        */
     }
 }
