3e7133604aebfff051416a1983658aae1151742a
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-03-02 10:03:56 +0800

    Update DBUtil.java

--- java_image_server/src/main/java/dao/DBUtil.java
+++ java_image_server/src/main/java/dao/DBUtil.java
@@ -8,6 +8,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ *  与数据库相关操作有关
+ */
 public class DBUtil {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/java_image_server?characterEncoding=utf8&useSSL=true";
     private static final String USERNAME = "root";
@@ -15,9 +18,14 @@ public class DBUtil {
 
     private static volatile DataSource dataSource = null;  //管理数据源
 
-    // 通过这个方法来创建 DataSource 的实例
+    /** （单例模式）
+     *  通过这个方法来创建一个 DataSource 的实例
+     * @return
+     */
     public static DataSource getDataSource() {
-        if (dataSource == null) {
+        if (dataSource == null) {   //双重校验锁
+            // 此处的 synchronized 其实就是给当前这一小块加锁
+            // synchronized 要么修饰一个方法，要么得传个对象，此处修饰整个方法的话锁的粒度太大
             synchronized (DBUtil.class) {
                 if (dataSource == null) {
                     dataSource = new MysqlDataSource();
@@ -31,15 +39,25 @@ public class DBUtil {
         return dataSource;
     }
 
+    /**
+     *  这个方法用来获取一个数据库的连接对象
+     * @return
+     */
     public static Connection getConnection() {
         try {
             return getDataSource().getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return null;   //出异常了返回 null
     }
 
+    /**
+     *  这个方法用来关闭连接
+     * @param connection
+     * @param statement
+     * @param resultSet
+     */
     public static void close(Connection connection, PreparedStatement statement, ResultSet resultSet) {
         try {
             if (resultSet != null) {
