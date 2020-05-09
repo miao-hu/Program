b6bd382b804e530c4e4a77759af38ae75cdc1b15
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-05-09 10:07:45 +0800

    Update begin.js

--- tangshikeshihua/src/main/webapp/js/begin.js
+++ tangshikeshihua/src/main/webapp/js/begin.js
@@ -1,4 +1,4 @@
-// var 是 variable
+// var 是 variable 变量的意思
 // document 是 js 中的 html 文档对象
 // document.getElementById('main')) 找到 html 文档中的 id 为 main 的元素
 // echarts.init 初始化画布元素
@@ -10,14 +10,17 @@ var option = {
     title: {
         text: 'ECharts 入门示例'
     },
+
     tooltip: {},
     legend: {
         data:['销量']
     },
+
     // 横坐标
     xAxis: {
         data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
     },
+
     yAxis: {},
     series: [{
         name: '销量',
@@ -30,6 +33,7 @@ var option = {
 myChart.setOption(option);
 
 
-/*   演示一下我选用的图表
-     当前的数据全部是在前端写死的，我需要通过一个 http 请求，从后端获取所有数据，并加入表格中
+/*   起初先演示一下我选用的图表
+     当前的数据全部是在前端写死的
+     接下来我需要通过一个 http 请求，从后端获取所有数据，并加入表格中
  */
